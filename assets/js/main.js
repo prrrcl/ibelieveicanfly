c2cdd986afbff4b395e24d1e9c45dd94d782f32b
prrrcl <prrrcl@MacBook-Pro-de-prrrcl.local>
2019-07-02 15:10:52 +0200

    Screen's created

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function main(){
+
+  var gameParent = document.querySelector('#game');
+
+  function buildDom(html){
+    gameParent.innerHTML = html;
+    return gameParent;
+  }
+
+  function createStartPage(){
+    var startScreen = buildDom(`
+      <section>
+        <header>
+          <h1>IBelieveICanFly &reg; The Game.</h1>
+        </header>
+        <button>Start</button>
+      </section>
+    `);
+    var startButton = startScreen.querySelector('button');
+    startButton.addEventListener('click', createGameScreen);
+  };
+
+  function createGameScreen(){
+    var gameScreen = buildDom(`
+      <section>
+        <canvas id="game-canvas" width="700" height="500"></canvas>
+      </section>
+    `);
+    var canvas = gameScreen.querySelector('#game-canvas');
+  }
+
+  function createGameOverScreen(){
+    var gameOverScreen = buildDom(`
+      <section>
+        <h2>GAME OVER </h2>
+        <button>Restart</button>
+      </section>
+    `);
+    var restartButton = gameOverScreen.querySelector('button');
+    restartButton.addEventListener('click', createGameScreen);
+  }
+
+  createStartPage();
+
+}
+window.addEventListener('load',main);
