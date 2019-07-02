d2adc6b7ba058cd8c5e1b592f6ac6552d6024df8
prrrcl <prrrcl@MacBook-Pro-de-prrrcl.local>
2019-07-02 15:42:58 +0200

    Added Game Loop

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -9,3 +9,27 @@ function Game(canvas){
   this.onGameOver = null;
 }
 
+Game.prototype.start = function(){
+  this.player = newPlayer (this.canvas);
+  var loop = () => {
+    if(Math.random() > 0.99){
+      var randomHeight = Math.random() * this.canvas.randomHeight - 10;
+      var newObstacle = new Obstacle(this.canvas, randomHeight);
+      this.obstacles.push(newObstacle);
+    }
+    // this.update();
+    // this.clear();
+    // this.draw();
+    // this.checkCollisions();
+    
+    if(!this.isGameOver){
+      var frameID = requestAnimationFrame(loop);
+    }else{
+      this.onGameOver();
+    }
+    
+   
+  }
+  loop();
+
+}
