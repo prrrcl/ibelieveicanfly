341c42493d89c83ed027685c4fc7a5fc30eab606
prrrcl <prrrcl@MacBook-Pro-de-prrrcl.local>
2019-07-02 15:43:36 +0200

    Added Player function

---
+++ assets/js/Player.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function Player(canvas) {
+  this.canvas = canvas;
+  this.ctxCanvas = this.canvas.getContext('2d');
+  this.height = 20;
+  this.width = 20;
+  this.x = 20;
+  this.y = (this.canvas.height / 2) - this.height / 2;
+  this.lives = 1;
+  this.velocity = 3;
+  this.direction = 0;
+  this.color = 'blue';
+}
+
+Player.prototype.move = function (){
+  this.y = this.y + this.direction * this.velocity;
+}
+Player.prototype.draw = function(){
+  this.ctxCanvas.fillStyle = this.color;
+  this.ctxCanvas.fillRect(this.x, this.y, this.width, this.height);
+}
+Player.prototype.setDirection = function(dir){
+  this.direction = dir;
+  this.move();
+}
